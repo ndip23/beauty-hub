e1e87b0f7e6a332c28e8f04e7ed6b212dc596808
obinna <giftotuuobina1995@gmail.com>
2026-05-31 00:17:23 +0100

    Update API URL to AWS Elastic Beanstalk backend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const isProduction = window.location.hostname !== "localhost";
 
 const API_BASE_URL = isProduction 
-  ? "https://beauty-heaven-api.vercel.app/"  
+  ? "http://Beautyhub-prod-env.eba-se9hjy5y.eu-north-1.elasticbeanstalk.com"  
   : "http://localhost:8000";                
 
 export const API = axios.create({ baseURL: API_BASE_URL });
